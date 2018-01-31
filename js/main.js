7133856c4f575df6d7d33e4ae822d3ba79f97b5c
nraovic <n.raovic@gmail.com>
2018-01-31 15:19:59 +0100

    Chanhe h1 to h3 for the restaurant title

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = "Restaurant image"
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
